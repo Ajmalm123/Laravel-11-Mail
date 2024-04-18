6fdfc64709b094c8703d741856039b3abbcbf4e2
Mohamed Ajmal <ajmal@mindster.in>
2024-04-18 18:38:13 +0530

    mail integration laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SendMailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('send/mail', [SendMailController::class, 'sendMailWithAttachment']);
